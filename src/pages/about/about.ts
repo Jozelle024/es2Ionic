b47d9c30fe6af7c5619d6c46699999620d360a83
Jozelle <jozellelauzon024@gmail.com>
2018-03-19 15:08:20 +0100

    battery status

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BatteryStatus, BatteryStatusResponse } from '@ionic-native/battery-status';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  batteryLevel: number;
+  isPlugged: boolean;
 
+  constructor(public navCtrl: NavController,
+              private batteryStatus: BatteryStatus) {
+    
+  }
+  ionViewDidLoad(){
+    this.batteryStatus.onChange().subscribe((status: BatteryStatusResponse) => {
+        console.log('ci sono');
+        console.log(status);
+        this.batteryLevel = status.level;
+        this.isPlugged = status.isPlugged;
+      }
+    );
   }
 
 }
