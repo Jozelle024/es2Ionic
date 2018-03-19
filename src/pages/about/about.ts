8a18c7fa17bc482cae1bcf919ddb56c2d96326ec
Jozelle <jozellelauzon024@gmail.com>
2018-03-19 17:12:59 +0100

    contact native storage

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { BatteryStatus, BatteryStatusResponse } from '@ionic-native/battery-status';
-import { NativeStorage } from '@ionic-native/native-storage';
-import { User } from '../../model/user';
 
 @Component({
   selector: 'page-about',
@@ -12,11 +10,9 @@ export class AboutPage {
 
   batteryLevel: number;
   isPlugged: boolean;
-  users: User[];
-  user: User;
+  
   constructor(public navCtrl: NavController,
-              private batteryStatus: BatteryStatus,
-              private nativeStorage: NativeStorage) {
+              private batteryStatus: BatteryStatus) {
     
   }
   ionViewDidLoad(){
@@ -27,20 +23,4 @@ export class AboutPage {
         this.isPlugged = status.isPlugged;
       });  
   }
-  getStorage(){
-    this.nativeStorage.getItem('users').then(
-      data => this.users = data,
-      error => console.error(error)
-    );
-  }
-  
-  setStorage(){
-    const newUser = new User();
-    if(this.users && newUser){
-      alert('set storage');
-      this.users.push(newUser);
-      this.nativeStorage.setItem('users', this.users);
-    }
-  }
-
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NativeStorage } from '@ionic-native/native-storage';
+import { User } from '../../model/user';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
-  constructor(public navCtrl: NavController) {
-
+  users: User[] = [];
+  user: User;
+  name: string;
+  lastname: string;
+  insertedUsers: User[] = [];
+  constructor(public navCtrl: NavController,
+              private nativeStorage: NativeStorage) {
+    this.user = new User();
+    this.user.name = '';
+    this.user.lastname = '';
+  }
+  ionViewDidLoad(){
   }
 
+  getStorage(){
+    this.nativeStorage.getItem('users').then(
+      data => this.insertedUsers = data,
+      error => alert(error)
+    );
+  }
+  
+  setStorage(){
+    if(this.users && this.user ){
+      const newUser = new User();
+      newUser.name = this.name;
+      newUser.lastname = this.lastname;
+      this.users.push(newUser);
+      this.nativeStorage.setItem('users', this.users);
+    }
+  }
 }
